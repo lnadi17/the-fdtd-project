4600ec850a8384c14e1c087b22ce88e6c2207b69
Luka Nadiradze <lnadi17@freeuni.edu.ge>
2023-05-17 23:38:29 +0400

    Add hard/soft source lines and prompt option

--- fdtd_1d.m
+++ fdtd_1d.m
@@ -49,7 +49,7 @@ ur = 1.0;
 
 % Time step (according to Courant Condition)
 dt = min(min(dx, dy), dz) / 2.0 / c0; % Single time step in seconds
-steps = 2000; % Total simulation steps
+steps = 3000; % Total simulation steps
 t = (0:dt:(steps - 1) * dt); % Each time step
 
 % Initialize materials to free space
@@ -70,11 +70,13 @@ e1 = 0; e2 = 0;
 
 %% Describe source
 fmax = 1e10; % Max frequency we're interested in
-tau = 0.5 / fmax; % Pulse width coefficient
+tau = 0.5 / fmax; % tau, aka FWHM (Full Width at Half Maximum)
 t0 = 6 * tau; % Pulse offset
 g = exp(-((t - t0) ./ tau ) .^ 2); % Create gaussian pulse
 
 %% Main FDTD Loop
+jump = 10;
+prompt = false;
 figure;
 
 for T = 1 : steps
@@ -83,22 +85,22 @@ for T = 1 : steps
     for nz = 1 : Nz - 1
         Hx(nz) = Hx(nz) + mHx(nz) * (Ey(nz+1) - Ey(nz));
     end
-    Hx(Nz) = Hx(Nz) + mHx(nz) * (e2 - Ey(Nz)); % Perfect boundary condition
+    Hx(Nz) = Hx(Nz) + mHx(nz) * (0 - Ey(Nz)); % Perfect boundary condition
 
 
     % Update E from H
     e2 = e1; e1 = Ey(Nz); % Record boundary E that we'll use 2 steps later
-    Ey(1) = Ey(1) + mEy(1) * (Hx(1) - h2); % Perfect boundary condition
+    Ey(1) = Ey(1) + mEy(1) * (Hx(1) - 0); % Perfect boundary condition
     for nz = 2 : Nz
         Ey(nz) = Ey(nz) + mEy(nz) * (Hx(nz) - Hx(nz-1));
     end
 
     % Inject source
     nzsrc = Nz / 2; % Source is in the middle
-    Ey(nzsrc) = Ey(nzsrc) + g(T);
+    Ey(nzsrc) = Ey(nzsrc) + g(T); % Soft Source
+    % Ey(nzsrc) = g(T); % Hard Source
 
     % Visualize E and H (not necessarily every step)
-    jump = 20;
     if mod(T, jump) == 0
         clf;
         hold on;
@@ -110,7 +112,7 @@ for T = 1 : steps
         pause(0.05);
     end
 
-    if T == 500
+    if T == 500 && prompt == true
         % Prompt the user to continue or stop
         answer = questdlg('Do you want to continue?', 'Continue Simulation', 'Continue', 'Stop', 'Continue');
 
