8eb6051f531b8a5022e530dff39f9022fbc33f83
Luka <lnadi17@freeuni.edu.ge>
2023-05-19 01:45:50 +0400

    Implement lossy material

--- fdtd_1d.m
+++ fdtd_1d.m
@@ -7,8 +7,8 @@
 % NOTE: If our simulation has critical dimensions, snapping grid to these
 % dimensions should be considered.
 
-Sz = 1; % Physical size along z
-Nz = 1000; % Number of cells along z
+Sz = 4; % Physical size along z
+Nz = 400; % Number of cells along z
 dz = Sz/Nz;
 
 %% Define Physical Parameters
@@ -24,19 +24,24 @@ ur = 1.0;
 
 % Time step (according to Courant Condition)
 dt = dz / 2.0 / c0; % Single time step in seconds
-steps = 10000; % Total simulation steps
+steps = 1000; % Total simulation steps
 t = (0:dt:(steps - 1) * dt); % Each time step
 
 % Initialize materials to free space
 ER = ones(1, Nz) .* er;
 UR = ones(1, Nz) .* ur;
+SIGMA = zeros(1, Nz);
 
 % Define geometry
-ER(Nz/2+1:Nz/2+400) = ones(1, 400) .* 4;
+ER(101:200) = ones(1, 100) .* 4;
+SIGMA(101:200) = ones(1, 100) .* 0.04;
 
-% Compute update coefficients (only for z direction)
-mEy = (c0 * dt) ./ ER / dz;
-mHx = (c0 * dt) ./ UR / dz;
+% Compute update coefficients
+EAF = dt * SIGMA ./ (2 * e0 .* ER);
+aEy = (1 - EAF) ./ (1 + EAF);
+bEy = (c0 * dt) ./ (ER .* (1 + EAF)) ./ dz;
+
+mHx = 0.5 ./ UR;
 
 % Initialize H and E fields to zeros
 Hx = zeros(1, Nz);
@@ -47,7 +52,7 @@ h1 = 0; h2 = 0;
 e1 = 0; e2 = 0;
 
 %% Describe source
-fmax = 1e10; % Max frequency we're interested in
+fmax = 1e9; % Max frequency we're interested in
 tau = 0.5 / fmax; % FWHM (Full Width at Half Maximum)
 t0 = 6 * tau; % Pulse offset
 th = t + dt + dt/2;
@@ -55,15 +60,15 @@ th = t + dt + dt/2;
 % These sources only work for Ey/Hx mode, and if the source is in the free space
 gaussian = exp(-((t - t0) ./ tau ) .^ 2); % Gaussian pulse
 gaussianH = -exp(-((th - t0) ./ tau) .^ 2); % Gaussian pulse for directional source H
-harmonic = 0.5 * sin(2 * pi * 1e9 * t); % 1 GHz harmonic source
-harmonicH = -0.5 * sin(2 * pi * 1e9 * th); % 1 GHz harmonic source for directional source H
+harmonic = 1 * sin(2 * pi * 700e6 * t); % 700 MHz harmonic source
+harmonicH = -1 * sin(2 * pi * 700e6 * th); % 700 MHz harmonic source for directional source H
 
 % Choose the desired pulse for simulation
 pulse = gaussian;
 pulseH = gaussianH;
 
 %% Main FDTD Loop
-jump = 20;
+jump = 1;
 prompt = false;
 figure;
 
@@ -77,17 +82,17 @@ for T = 1 : steps
 
     % Update E from H
     e2 = e1; e1 = Ey(Nz); % Record boundary E that we'll use 2 steps later
-    Ey(1) = Ey(1) + mEy(1) * (Hx(1) - h2); % Perfect boundary condition
+    Ey(1) = aEy(1) * Ey(1) + bEy(1) * (Hx(1) - h2); % Perfect boundary condition
     for nz = 2 : Nz
-        Ey(nz) = Ey(nz) + mEy(nz) * (Hx(nz) - Hx(nz-1));
+        Ey(nz) = aEy(nz) * Ey(nz) + bEy(nz) * (Hx(nz) - Hx(nz-1));
     end
 
     % Inject source
-    nzsrc = Nz / 4; % Source is at the quarter of the length
+    nzsrc = 5; % Source is in the fifth cell
     
     % Directional source
     Hx(nzsrc - 1) = Hx(nzsrc - 1) - mHx(nzsrc - 1) * pulse(T);
-    Ey(nzsrc) = Ey(nzsrc) - mEy(nzsrc) * pulseH(T);
+    Ey(nzsrc) = Ey(nzsrc) - bEy(nzsrc) * pulseH(T);
 
     % Soft source
     % Ey(nzsrc) = Ey(nzsrc) + pulse(T);
@@ -103,8 +108,8 @@ for T = 1 : steps
         clf;
         hold on;
         plot(Ey, 'LineWidth', 2);
-        plot(Hx, 'LineWidth', 2);
-        rectangle('Position', [Nz/2, -1.5, 400, 3], 'FaceColor', [0, 0, 1, 0.3], 'EdgeColor', 'none');
+        % plot(Hx, 'LineWidth', 2);
+        rectangle('Position', [100, -1.5, 100, 3], 'FaceColor', [0, 0, 1, 0.3], 'EdgeColor', 'none');
         ylim([-1.5, 1.5]);
         xlim([0, Nz]);
         title(['Step ', num2str(T)])
