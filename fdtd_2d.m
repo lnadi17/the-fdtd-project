87c98a95d077e3e5ccc8d12d66fa445d562980d9
Luka <lnadi17@freeuni.edu.ge>
2023-05-25 01:45:56 +0400

    Add a second circular source

--- fdtd_2d.m
+++ fdtd_2d.m
@@ -107,7 +107,7 @@ for T = 1 : steps
 
     % Compute CEy
     CEy(1:Nx-1,1:Ny) = -1*(Ez(2:Nx,1:Ny) - Ez(1:Nx-1,1:Ny))/dx;
-    CEy(Nx,1:Ny) = (Ez(Nx,1:Ny))/dx;
+    CEy(Nx,1:Ny) = Ez(Nx,1:Ny)/dx;
 
     % Update H integrations
     ICEx = ICEx + CEx;
@@ -136,13 +136,26 @@ for T = 1 : steps
     freq = 1e9;
     lambda = c0/freq;
     radius = lambda;
-    center = [Nx/2, Ny/2];
+    center1 = [Nx/2, Ny/2 + 1.5 * radius/dx];
+    center2 = [Nx/2, Ny/2 - 1.5 * radius/dx];
     for i = 1 : Nx
         for j = 1 : Ny
-            dist = sqrt(((center(1) - i)*dx)^2 + ((center(2) - j)*dy)^2);
-            if abs(dist - radius) <= 3 * dx
+            dist1 = sqrt(((center1(1) - i)*dx)^2 + ((center1(2) - j)*dy)^2);
+            dist2 = sqrt(((center2(1) - i)*dx)^2 + ((center2(2) - j)*dy)^2);
+            if abs(dist1 - radius) <= 3 * dx
                 % Calculate angle in degrees (-180 to 180)
-                deg = rad2deg(angle((i - center(1)) + 1j * (j - center(2))));
+                deg = rad2deg(angle((i - center1(1)) + 1j * (j - center1(2))));
+                phase = deg / 2;
+                amplitude = -(deg/180)^2 + 1.5;
+                sigmoid = 1 ./ (1 + exp(-0.1*(T - 50)));
+
+                % Inject hard source (simulating metal)
+                pulse = 10 * sin(2 * pi * freq * T * dt + phase);
+                Dz(i, j) = amplitude * sigmoid * pulse;
+            end
+            if abs(dist2 - radius) <= 3 * dx
+                % Calculate angle in degrees (-180 to 180)
+                deg = rad2deg(angle((i - center2(1)) + 1j * (j - center2(2))));
                 phase = deg / 2;
                 amplitude = -(deg/180)^2 + 1.5;
                 sigmoid = 1 ./ (1 + exp(-0.1*(T - 50)));
@@ -155,7 +168,7 @@ for T = 1 : steps
     end
 
     % Calculate Ez Field
-    Ez = mEz1.*Dz;
+    Ez = mEz1 .* Dz;
 
     if mod(T, 1) == 0
         imagesc(xa, ya, Dz');
