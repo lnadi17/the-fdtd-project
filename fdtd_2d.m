7893d319017cb53c8e8432890aacb20718cdc60c
Luka <lnadi17@freeuni.edu.ge>
2023-05-24 16:06:08 +0400

    Add PECs as walls, polish plotting

--- fdtd_2d.m
+++ fdtd_2d.m
@@ -1,5 +1,5 @@
 %% Define Grid
-Nx = 60; Ny = 60;
+Nx = 100; Ny = 100;
 dx = 0.01;
 dy = 0.01;
 Sx = Nx*dx;
@@ -28,48 +28,53 @@ t0 = 20;
 spread = 6.0;
 
 % Simulation
-steps = 200;
+steps = 500;
 
 %% Main Loop
 for T = 1 : steps
     % Calculate Dz Field
-    for i = 2 : Nx - 1
-        for j = 2 : Ny - 1
+    for i = 2 : Nx
+        for j = 2 : Ny
             Dz(i, j) = Dz(i, j) + 0.5 * (Hy(i, j) - Hy(i-1, j) - Hx(i, j) + Hx(i, j-1));
         end
-        Dz(i, 1) = Dz(i, 1) + 0.5 * (Hy(i, 1) - Hy(i-1, 1) - Hx(i, 1) + 0);
     end
-    Dz(1, 1) = Dz(1, 1) + 0.5 * (Hy(1, 1) - 0 - Hx(1, 1) + 0);
 
     % Put a Gaussian pulse in the middle
-    pulse = exp(-0.5*(((t0 - T) / spread)^2));
+    pulse = 10 * exp(-0.5*(((t0 - T) / spread)^2));
     Dz(Nx/2, Ny/2) = Dz(Nx/2, Ny/2) + pulse;
 
     % Calculate Ez Field
-    for i = 1 : Nx - 1
-        for j = 1 : Ny - 1
+    for i = 1 : Nx
+        for j = 1 : Ny
             Ez(i, j) = Ga(i, j) * Dz(i, j);
         end
     end
 
     % Calculate Hx Field
     for i = 1 : Nx - 1
-        for j = 1 : Ny - 2
+        for j = 1 : Ny - 1
             Hx(i, j) = Hx(i, j) + 0.5 * (Ez(i, j) - Ez(i, j+1));
         end
-        Hx(i, Ny-1) = Hx(i, Ny-1) + 0.5 * (Ez(i, Ny-1) - 0);
     end
     
 
     % Calculate Hy Field
-    for i = 1 : Nx - 2
+    for i = 1 : Nx - 1
         for j = 1 : Ny - 1
             Hy(i, j) = Hy(i, j) + 0.5 * (Ez(i+1, j) - Ez(i, j));
         end
     end
-    Hy(Nx-1, Ny-1) = Hy(Nx-1, Ny-1) + 0.5 * (0 - Ez(Nx-1, Ny-1));
+
+    % PECs at the boundaries
+    Dz(1, :) = 0;
+    Dz(Nx, :) = 0;
+    Dz(:, 1) = 0;
+    Dz(:, Ny) = 0;
 
     imagesc(xa, ya, Dz');
+    colormap 'jet';
+    clim([-1 1]);
+    colorbar;
     pause(0.1);
 end
 
